5d1ba04ec55230d087404bca035844f2cd7b56ab
SusanJake <susan.csb2024@saintgits.org>
2024-11-10 16:56:17 +0530

    added bargraph and progress bar

--- src/pages/ManagerHome.jsx
+++ src/pages/ManagerHome.jsx
@@ -1,14 +1,26 @@
 import { useEffect, useState } from 'react';
-import { Card, Divider, Segmented } from 'antd';
+import { Card, Divider, Segmented, Progress, Popover, Layout, Row, Col } from 'antd';
 import Timeline from '../pages/Timeline';
 import CardFlip from '../pages/CardFlip';
 import { SendApiRequest } from '../framework/api';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip } from "recharts";
 
 export default function Dashboard() {
     const [classroomsData, setClassroomData] = useState([]);
     const [timelineData, setTimelineData] = useState([]);
-    const [selectedClassroom,setSelectedClassroom] = useState();
-    const [statistics,setStatistics] = useState({});
+    const [selectedClassroom, setSelectedClassroom] = useState();
+    const [statistics, setStatistics] = useState({});
+
+    const data = [
+        { name: "Page A", uv: 4000 },
+        { name: "Page B", uv: 3000 },
+        { name: "Page C", uv: 2000 },
+        { name: "Page D", uv: 2780 },
+        { name: "Page E", uv: 1890 },
+        { name: "Page F", uv: 2390 },
+        { name: "Page G", uv: 3490 },
+    ];
+
     // Fetch classroom data on component mount
     useEffect(() => {
         async function managerDashboardData() {
@@ -17,7 +29,8 @@ export default function Dashboard() {
                 authenticated: true,
             });
             setStatistics(data);
-            setClassroomData(data['classes']); // Update classrooms data after fetch
+            setClassroomData(data['classes']);
+            setSelectedClassroom(data['classes'][0])
         }
         managerDashboardData();
     }, []);
@@ -25,22 +38,19 @@ export default function Dashboard() {
     // Generate timeline data whenever classroomsData is updated
     useEffect(() => {
         if (selectedClassroom && classroomsData.length > 0) {
-            
-            if (selectedClassroom) {
-                const newTimelineData = [
-                    {
-                        heading: 'Start Date',
-                        subHeading: selectedClassroom.start_date,
-                        direction: 'left'
-                    },
-                    {
-                        heading:'End Date' ,
-                        subHeading: selectedClassroom.eod,
-                        direction: 'right'
-                    }
-                ];
-                setTimelineData(newTimelineData);
-            }
+            const newTimelineData = [
+                {
+                    heading: 'Start Date',
+                    subHeading: selectedClassroom.start_date,
+                    direction: 'left'
+                },
+                {
+                    heading: 'End Date',
+                    subHeading: selectedClassroom.eod,
+                    direction: 'right'
+                }
+            ];
+            setTimelineData(newTimelineData);
         }
     }, [selectedClassroom]);
 
@@ -48,27 +58,67 @@ export default function Dashboard() {
         <>
             <h1 className='dashboardHeader'>Dashboard</h1>
             <Divider className='dashboardDivider' />
-            
-            <CardFlip statistics={statistics}/>
+
+            <CardFlip statistics={statistics} />
             <Divider />
-            <h1>Course Timelines</h1>
-            <Card >
-                <Segmented
-                    options={classroomsData.map((classroom) => classroom.title)}
-                    onChange={(value) => {
-                        const classroom = classroomsData.find((cls) => cls.title === value);
-                        // Handle classroom selection if needed
-                        setSelectedClassroom(classroom);
-                        console.log("Selected classroom is",selectedClassroom)
-                    }}
-                />
-                <div style={{display:'flex',justifyContent:'space-between',alignItems:'center',width:'50vw',backgroundColor:'aliceblue'}}>
-                    <Timeline eventData={timelineData} />
-                </div>
-                <div>
-                    HELLO
-                </div>
-            </Card>
+            {/* <h1>Course Timelines</h1> */}
+            <Layout>
+                <Row>
+
+                    {/* Timeline and Progress chart */}
+                    <Col span={12}>
+                        <div>
+                            <Segmented
+                                //an array of titles
+                                options={classroomsData.map((classroom) => classroom.title)}
+                                onChange={(value) => {
+                                    const classroom = classroomsData.find((cls) => cls.title === value);
+                                    setSelectedClassroom(classroom);
+                                }}
+                                value={selectedClassroom?.title}
+                        
+                            />
+                            <Row>
+                                <Col span={18}>
+                                    <Timeline eventData={timelineData} />
+                                </Col>
+                                <Col span={6}>
+                                    <Popover content={<p>Completion percentage with respect to the deadline</p>} title="Completion percentage">
+                                        <Progress
+                                            type="dashboard"
+                                            steps={5}
+                                            percent={parseInt(selectedClassroom?.completion_timeline)}
+                                            trailColor="rgba(234, 85, 85, 0.8)"
+                                            strokeWidth={20}
+                                            size="small"
+
+                                        />
+                                    </Popover>
+                                </Col>
+
+                            </Row>
+                        </div>
+                    </Col>
+                    {/* Bar Chart */}
+                    <Col span={12}>
+                        <div className='BarchartDiv' style={{ flex: '1', backgroundColor: '' }}>
+                            <BarChart
+                                width={500}
+                                height={400}
+                                data={data}
+                                barSize={40}
+                                margin={{ right: 4 }}
+                            >
+                                <XAxis />
+                                <YAxis />
+                                <CartesianGrid strokeDasharray="5 5" />
+                                <Tooltip />
+                                <Bar dataKey="uv" fill="#8884d8" animationDuration={5000} animationEasing="ease-in" />
+                            </BarChart>
+                        </div>
+                    </Col>
+                </Row>
+            </Layout>
         </>
     );
 }
